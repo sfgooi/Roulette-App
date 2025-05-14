b5016de1fcd829053ee3f48b399be4643f55cb3d
内田　滉二 <kouji.uchida@eras.jp>
2025-05-14 23:13:55 +0900

    SettingsModalのリファクタリング

--- src/components/molecules/SettingsModal.tsx
+++ src/components/molecules/SettingsModal.tsx
@@ -1,15 +1,29 @@
 import { Modal, Box } from "@mui/material";
+import styled from "@emotion/styled";
 import MemberTable from "../organisms/MemberTable";
 import { Member } from "../../api/dynamoDB/types";
 
-type SettingsModalProps = {
+const StyledBox = styled(Box)({
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  maxWidth: 800,
+  backgroundColor: "white",
+  boxShadow: "0px 4px 24px rgba(0, 0, 0, 0.25)",
+  padding: "32px",
+  borderRadius: "16px",
+});
+
+type Props = {
   open: boolean;
   onClose: () => void;
   members: Member[];
   setMembers: React.Dispatch<React.SetStateAction<Member[]>>;
 };
 
-const SettingsModal: React.FC<SettingsModalProps> = ({
+const SettingsModal: React.FC<Props> = ({
   open,
   onClose,
   members,
@@ -17,22 +31,13 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
 }) => {
   return (
     <Modal open={open} onClose={onClose} aria-labelledby="settings-modal">
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: "80%",
-          maxWidth: 800,
-          bgcolor: "background.paper",
-          boxShadow: 24,
-          p: 4,
-          borderRadius: 2,
-        }}
-      >
-        <MemberTable onClose={onClose} members={members} setMembers={setMembers} />
-      </Box>
+      <StyledBox>
+        <MemberTable
+          onClose={onClose}
+          members={members}
+          setMembers={setMembers}
+        />
+      </StyledBox>
     </Modal>
   );
 };
